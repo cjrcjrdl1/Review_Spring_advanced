065741b0396986e6e12cb5b573b8b3fb8f9ca1eb
cjrcjrdl1 <cjrcjrdl1@naver.com>
2022-07-30 09:42:28 +0900

    템플릿 메서드 패턴 - 예제2

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -25,6 +25,31 @@ public class TemplateMethodTest {
 
     }
 
+    @Test
+    void templateMethod2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        log.info("클래스 이름1={}", template1.getClass());
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        log.info("클래스 이름2={}", template2.getClass());
+        template2.execute();
+
+
+    }
+
     private void logic1() {
         long startTime = System.currentTimeMillis();
         //비즈니스 로직 실행
